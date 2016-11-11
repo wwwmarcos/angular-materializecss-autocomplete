45baed97098ef23468cc4480d02316df8b5023ce
dstepbw <dstepbw@gmail.com>
2016-11-11 09:18:24 -0200

    create basic options

--- angular-materializecss-autocomplete.js
+++ angular-materializecss-autocomplete.js
@@ -1,40 +1,155 @@
-(function() {
+(function () {
   'use strict'
 
-   angular
+  angular
     .module('angularMaterializeAutoComplete', [])
     .directive('autoComplete', autoComplete)
+    .directive('autoCompleteViewValue', autoCompleteViewValue)
     .run(run)
 
-    run.$inject = ['$templateCache']
-    function run($templateCache){
-      $templateCache
-        .put('angular-materializecss-autocomplete.html', 
-               '<div class="row">'
-         +     '<div class="col s12">'
-         +       '<div class="row">'
-         +         '<div class="input-field col s12">'
-         +           '<i class="material-icons prefix">textsms</i>'
-         +           '<input type="text" id="autocomplete-input" class="autocomplete">'
-         +           '<label for="autocomplete-input">Autocomplete</label>'
-         +         '</div>'
-         +       '</div>'
-         +     '</div>'
-         +   '</div>')
+  run.$inject = ['$templateCache']
+  function run($templateCache) {
+    $templateCache
+      .put('angular-materializecss-autocomplete.template.html',
+        '<ng-form name="autoCompleteForm">'
+      + '	<div class="row">'
+      + '		<div class="input-field">'
+      + '			<i class="material-icons prefix" data-ng-if="iconPrefixDefined()">{{iconPrefix}}</i>'
+      + '			<input type="text" auto-complete-view-value="{{property}}" validate-obj="{{validadeObject}}" data-ng-model="model" class="autocomplete"'
+      + '				data-ng-change="modelChanged(model)" />'
+      + '			<label for="autocomplete-input">{{label}}</label>'
+      + '			<ul class="autocomplete-content dropdown-content">'
+      + '				<li data-ng-repeat="item in itens track by $index" data-ng-if="match(item[property], model) && !closed" data-ng-click="select(item, model)">'
+      + '					<span>'
+      + '              <!--<span class="highlight">{{item[property]}}</span>--> {{item[property]}}'
+      + '					<!--<span class="highlight">{{ buildItemDescription(item[property], model, true) }}</span>{{ buildItemDescription(item[property], model, false) }}-->'
+      + '					</span>'
+      + '				</li>'
+      + '			</ul>'
+      + '		</div>'
+      + '	</div>'
+      + '</ng-form>')
+  }
+
+  function autoComplete() {
+    var directive = {
+      restrict: 'E',
+      scope: {
+        model: '=',
+        label: '@',
+        func: '&',
+        iconPrefix: '@',
+        property: '@',
+        onModelChanged: '&',
+        onSelect: '&',
+        validadeObject: '@',
+        limit: '@'
+      },
+      link: link,
+      templateUrl: 'angular-materializecss-autocomplete.template.html'
     }
+    return directive
 
-    function autoComplete(){
-      var directive = {
-        restrict: 'E',
-        scope: {},
-        link: link,
-        templateUrl: 'angular-materializecss-autocomplete.html'
+    function link(scope, element, attrs) {
+
+      scope.func = scope.func()
+      scope.onModelChanged = scope.onModelChanged()
+      scope.onSelect = scope.onSelect()
+
+      scope.closed = false
+      // in progress
+      scope.limit = scope.limit || 10
+
+      scope.iconPrefixDefined = iconPrefixDefined
+      scope.modelChanged = modelChanged
+      scope.match = match
+      scope.select = select
+      scope.buildItemDescription = buildItemDescription
+
+      function modelChanged(model) {
+        scope.closed = false
+        scope.itens = scope.func()
+        callIfDefined(scope.onModelChanged, model)
       }
-      return directive
 
-      function link (scope, element, attrs) {
-
+      function iconPrefixDefined() {
+        return angular.isDefined(scope.iconPrefix)
       }
 
+      function match(item, model) {
+        if (!angular.isObject(model)) {
+          return (item.toUpperCase()).indexOf(model.toUpperCase()) != -1
+        }
+      }
+
+      function select(item, model) {
+        scope.model = item
+        scope.closed = true
+        callIfDefined(scope.onSelect, item, model)
+      }
+
+      function callIfDefined(func, argumentOne, argumentTwo) {
+        if (func) {
+          func(argumentOne, argumentTwo)
+        }
+      }
+
+      // in progress
+      function buildItemDescription(item, model, isHighlightText) {
+        if (isHighlightText) {
+          var rg = new RegExp(item, "g")
+          return model.replace(rg, '')
+        }
+        var rg = new RegExp(model, "g")
+        return item.replace(rg, '')
+      }
+
+      function validadeObject(model) {
+        if (!angular.isObject(model) && scope.autoCompleteForm.$dirty) {
+          return setValidity('invalidObject', false)
+        }
+      }
+
+      function setValidity(key, isValid) {
+        ngForm.$setValidity(key, isValid)
+      }
+
+      // in progress
+      element.bind('blur', function () {
+        if (scope.validadeObject != false || scope.validadeObject != 'false') {
+          validadeObject()
+        }
+      })
     }
+  }
+
+  function autoCompleteViewValue() {
+    var directive = {
+      restrict: 'A',
+      scope: {
+        ngModel: '=',
+        autoCompleteViewValue: '@',
+      },
+      link: link
+    }
+    return directive
+
+    function link(scope, element, attrs, ngForm) {
+
+      scope.$watch('ngModel', function (value) {
+        if (value && angular.isObject(value)) {
+          setInputViewValue(value[getVisibleProperty()])
+        }
+      })
+
+      function getVisibleProperty() {
+        return scope.autoCompleteViewValue
+      }
+
+      function setInputViewValue(value) {
+        return element.val(value)
+      }
+    }
+  }
+
 })()
--- index.controller.js
+++ index.controller.js
@@ -7,5 +7,32 @@
 
     function IndexController(){
       var vm = this
+      
+      var cities = [
+        {
+          name: 'Maringa', uf: 'PR', id: '500c709b-9504-462e-9b24-422852cac418'
+        },
+        {
+          name: 'São Paulo', uf: 'SP', id: '6292ad1a-e80f-4642-9bb7-0c19f10db5b1'
+        },
+        {
+          name: 'Apucarana', uf: 'PR', id: '7aebb3b6-c7ae-42aa-8d11-7b5ecc36b6f1'
+        },
+        {
+          name: 'Londrina', uf: 'PR', id: '560c2ebe-b59f-44b2-99a3-19e9f148e401'
+        },
+        {
+          name: 'Curitiba', uf: 'PR', id: '2b97cb57-46a3-4733-a69b-068f0a3b31ab'
+        }
+      ]
+      
+      vm.getCities = getCities
+
+      //////////////////////////////
+
+      function getCities(){
+        return cities
+      }
+
     }
 })()
